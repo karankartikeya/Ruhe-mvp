81a07df75a9498260847975ad129d7d11ea4ef0d
Karan Kartikeya <karankartikey72@gmail.com>
2025-01-03 22:33:08 +0100

    fix: meta tag for viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default async function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
