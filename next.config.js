0fd23d288e2e507f10c6cefa97a93e4fb5f08319
Karan Kartikeya <karankartikey72@gmail.com>
2025-01-05 23:40:41 +0100

    fi: pwa removed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
-});
-
-const nextConfig = withPWA({
-  images: {
+const nextConfig = {
+   images: {
     domains: ["lh3.googleusercontent.com"],
   },
   reactStrictMode: false,
-});
-
-module.exports = nextConfig;
+  };
+  
+  module.exports = nextConfig;
+  
