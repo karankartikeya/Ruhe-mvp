7cda98423a8f8251889245d0192a450956f752d2
Karan Kartikeya <karankartikey72@gmail.com>
2025-01-21 16:15:57 +0100

    remove unused scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,16 +30,6 @@ export default async function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <script src="https://cdn.jsdelivr.net/npm/@editorjs/simple-image"></script>
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <script
-          type="text/javascript"
-          src="https://maps.googleapis.com/maps/api/js?v=3.exp"
-        ></script>
       </head>
       <body>
         {/* <SessionWrapper session={session}> */}
